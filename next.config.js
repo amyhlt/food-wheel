47ff0e2c073348d7120a60d03dd4fc3587f2038e
amyhlt <huilitang1@gmail.com>
2026-05-01 15:20:11 +0800

    restore full project with api routes

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 
 // next.config.js
+
 const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development' // 开发环境禁用，生产环境启用
+  disable: process.env.NODE_ENV === 'development'
 })
 
 module.exports = withPWA({
